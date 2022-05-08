98395f417b00721fbc15f1a8fc1c76ec7297739b
Cols-T3RM1N4L <105165862+Cols-T3RM1N4L@users.noreply.github.com>
2022-05-08 23:20:44 +0700

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,21 @@
+import requests
+import time
+
+
+# logo
+print ('------------------------------')
+print ('[+] SCRIPT : COLS-T3RM1NAL')
+print ('[+] YOUTUBE : COLS SCRIPT')
+print ('[+] INSTAGRAM : COLS9999999')
+print ('------------------------------')
+print ('\n[+] NOMOR DIAWALI 8xxxx')
+
+
+# run nomor
+nomor = input('[+] NOMOR TARGET : ')
+req=requests.get('https://raw.githubusercontent.com/Cvar1984/LiteOTP/master/build/lite', 'lite')
+if 'terkirim' in req:
+     print ('\n[✓] spam terkirim')
+else:
+     print ('\n[!] spam gagal')
+
